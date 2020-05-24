19a9e31f72e9dac1beff4b9418cbff5ac0f66a07
lokesh8389 <41296373+lokesh8389@users.noreply.github.com>
2020-05-24 14:27:03 +0530

    git and Dockerfile

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, json
+
+companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
+
+api = Flask(__name__)
+
+@api.route('/companies', methods=['GET'])
+def get_companies():
+  return json.dumps(companies)
+
+@api.route('/user/<name>', methods=['GET'])
+def hello(name):
+  return 'Hello, My name is %s !' % name
+
+if __name__ == '__main__':
+    api.run()
